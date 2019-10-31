a5ad7a1776888409714a05cf718963283bb85675
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2019-10-31 18:08:46 -0400

    added user model class

---
+++ ReaderKnight_API/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReaderKnight_API.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public int Pages { get; set; }
+        public double Price { get; set; }
+    }
+}
